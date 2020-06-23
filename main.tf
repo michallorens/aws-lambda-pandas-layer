76fda03894113ecd3bb984adcc5fda704e092728
michallorens <michallorens@gmail.com>
2020-06-23 10:01:08 +0200

    Hosting zipped archives on S3 to accomodate packages larger than 50MB.

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   clean = "rm -rf ${path.module}/build/libs"
-  build = "pip install --upgrade ${join(" ", var.libraries)} --target ${path.module}/build/libs/python/lib/python${data.external.python-version.result.version}/site-packages/."
+  build = "pip install --upgrade ${join(" ", var.libraries)} --target ${path.module}/build/libs/python/lib/python${data.external.default.result.version}/site-packages/."
 }
 
 resource null_resource default {
@@ -14,14 +14,27 @@ resource null_resource default {
 }
 
 data archive_file default {
-  output_path = "${path.module}/build/aws-lambda-python-layer-${null_resource.default.id}.zip"
+  output_path = "${path.module}/build/aws-lambda-python-layer.zip"
   source_dir  = "${path.module}/build/libs"
   type        = "zip"
 
   depends_on  = [null_resource.default]
 }
 
-data external python-version {
+resource aws_s3_bucket default {
+  bucket = var.bucket
+  acl    = "private"
+}
+
+resource aws_s3_bucket_object default {
+  bucket = aws_s3_bucket.default.bucket
+  source = data.archive_file.default.output_path
+  key    = "python-layer-${random_id.default.b64_url}.zip"
+
+  etag   = data.archive_file.default.output_base64sha256
+}
+
+data external default {
   program = ["bash", "${path.module}/python_version.sh"]
 }
 
@@ -30,10 +43,13 @@ resource random_id default {
 }
 
 resource aws_lambda_layer_version default {
-  filename   = data.archive_file.default.output_path
+  s3_bucket  = aws_s3_bucket.default.bucket
+  s3_key     = aws_s3_bucket_object.default.key
   layer_name = "python-layer-${random_id.default.b64_url}"
 
+  source_code_hash = data.archive_file.default.output_base64sha256
+
   compatible_runtimes = [
-    "python${data.external.python-version.result.version}"
+    "python${data.external.default.result.version}"
   ]
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable libraries {
   type        = list(string)
   description = "A list of python libraries to download."
 }
+
+variable bucket {
+  type        = string
+  description = "S3 bucket for storing zipped libraries"
+}
