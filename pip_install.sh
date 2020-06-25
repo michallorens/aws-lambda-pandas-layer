9c165f4ccdbc6c753d173f827da2840c2a2023d4
michallorens <michallorens@gmail.com>
2020-06-25 12:36:11 +0200

    Fixed pip install directory

--- pip_install.sh
+++ pip_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-DIR="/pip/libs/python/lib/python$PYTHON_VERSION/site-packages"
+DIR="/libs/python/lib/python$PYTHON_VERSION/site-packages"
 pip3 install "$@" --target "$DIR"
 zip -r libraries.zip pip/
--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-rm -rf "$(dirname "${BASH_SOURCE[0]}")"/build
-mkdir "$(dirname "${BASH_SOURCE[0]}")"/build
+DIR="$(dirname "${BASH_SOURCE[0]}")"/build
+rm -rf "$DIR"
+mkdir "$DIR"
 docker run --name build-lambda-layer "$@" > /dev/null 2>&1
-docker cp build-lambda-layer:/libraries.zip "$(dirname "${BASH_SOURCE[0]}")"/build/libraries.zip > /dev/null 2>&1
+docker cp build-lambda-layer:/libraries.zip "$DIR"/libraries.zip > /dev/null 2>&1
 docker rm build-lambda-layer > /dev/null 2>&1
-jq -n "{ path: \"$(dirname "${BASH_SOURCE[0]}")/build/libraries.zip\" }"
+jq -n "{ path: \"$DIR/libraries.zip\" }"
