4c1f3f72506c5ec217fc8bb483af900f4793cbb8
michallorens <michallorens@gmail.com>
2020-06-25 12:17:24 +0200

    Fixed relative build paths

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm -rf build
-mkdir build
+rm -rf "$(dirname "${BASH_SOURCE[0]}")"/build
+mkdir "$(dirname "${BASH_SOURCE[0]}")"/build
 docker run --name build-lambda-layer "$@" > /dev/null 2>&1
-docker cp build-lambda-layer:/libraries.zip build/libraries.zip > /dev/null 2>&1
+docker cp build-lambda-layer:/libraries.zip "$(dirname "${BASH_SOURCE[0]}")"/build/libraries.zip > /dev/null 2>&1
 docker rm build-lambda-layer > /dev/null 2>&1
 jq -n "{ path: \"$(dirname "${BASH_SOURCE[0]}")/build/libraries.zip\" }"
