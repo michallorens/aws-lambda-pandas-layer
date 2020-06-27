5af14ebb217f0d3b82c05f1b0afb58987adaa882
michallorens <michallorens@gmail.com>
2020-06-27 23:56:25 +0200

    Randomized container name

--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 DIR="$(dirname "${BASH_SOURCE[0]}")"/build
+CONTAINER_ID=build-lambda-layer-$(xxd -l 16 -p /dev/urandom | tr -d " \n")
 rm -rf "$DIR"
 mkdir "$DIR"
-docker run --name build-lambda-layer "$@" > /dev/null 2>&1
-docker cp build-lambda-layer:/libraries.zip "$DIR"/libraries.zip > /dev/null 2>&1
-docker rm build-lambda-layer > /dev/null 2>&1
+docker run --name "$CONTAINER_ID" "$@" > /dev/null 2>&1
+docker cp "$CONTAINER_ID":/libraries.zip "$DIR"/libraries.zip > /dev/null 2>&1
+docker rm "$CONTAINER_ID" > /dev/null 2>&1
 jq -n "{ path: \"$DIR/libraries.zip\" }"
