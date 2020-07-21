081c9a25a0caf3bfedd410b8e10231f297c5d169
michallorens <michallorens@gmail.com>
2020-07-21 14:09:54 +0200

    Removing packages already included in lambda runtime

--- pip_install.sh
+++ pip_install.sh
@@ -5,4 +5,11 @@ find "$DIR" -name "*.so" | xargs strip -s
 find "$DIR" -wholename "**/*.py[c|o]" | xargs rm -rf
 find "$DIR" -wholename "**/__pycache__*" | xargs rm -rf
 find "$DIR" -wholename "**/*.dist-info*" | xargs rm -rf
+find "$DIR" -type d -name "boto3" | xargs rm -rf
+find "$DIR" -type d -name "botocore" | xargs rm -rf
+find "$DIR" -type d -name "dateutil" | xargs rm -rf
+find "$DIR" -type d -name "distutils" | xargs rm -rf
+find "$DIR" -type d -name "docutils" | xargs rm -rf
+find "$DIR" -type d -name "pkg_resources" | xargs rm -rf
+find "$DIR" -type d -name "setuptools" | xargs rm -rf
 zip -r /libraries.zip python
