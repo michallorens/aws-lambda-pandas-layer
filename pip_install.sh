52a3cc1aef0caafaa2a828905fcea4d315be4d14
michallorens <michallorens@gmail.com>
2020-07-21 12:47:24 +0200

    Downsizing python packages

--- pip_install.sh
+++ pip_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 DIR="python/lib/python$PYTHON_VERSION/site-packages"
 pip3 install "$@" --target "$DIR"
+find "$DIR" -name "*.so" | xargs strip -s
+find "$DIR" -wholename "**/*.py[c|o]" | xargs rm -rf
+find "$DIR" -wholename "**/__pycache__*" | xargs rm -rf
+find "$DIR" -wholename "**/*.dist-info*" | xargs rm -rf
 zip -r /libraries.zip python
